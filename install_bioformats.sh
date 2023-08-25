b2f212308d3da52447d30b4e3c240f501c44c3d1
CGDogan <126820728+CGDogan@users.noreply.github.com>
2023-08-25 16:12:20 +0100

    Don't manually install logger

--- install_bioformats.sh
+++ install_bioformats.sh
@@ -18,22 +18,10 @@ cd ..
 rm -r temp
 rm bioformats_all_in_one_compressed.jar
 
-# remove this the following lines to print all debug info
-# link can be found by searching slf4j simple jar 1.X (not 2.X as of BioFormats 7)
-wget -q https://repo1.maven.org/maven2/org/slf4j/slf4j-simple/1.7.36/slf4j-simple-1.7.36.jar -O logger_printing_errors_only_compressed.jar
-
-# likewise
-unzip -q logger_printing_errors_only_compressed.jar -d temp
-cd temp
-zip -0 -q -r ../logger_printing_errors_only.jar *
-cd ..
-rm -r temp
-rm logger_printing_errors_only_compressed.jar
-
 # to verify jar contents, you may do:
 # jar tvf ../jar_files/bioformats_all_in_one.jar
 
 ## Move them to a single directory
 mv /root/src/image-decoders/bioformats/* /usr/lib/java
 
-#### Missing optimization: Packing all Jars to a single Jar
+#### Missing optimization: Packing our Jar and BioFormats Jar to a single Jar
