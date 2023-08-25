1339e3d9b9bb0b55485d7430e66a1c9f380e88e2
CGDogan <126820728+CGDogan@users.noreply.github.com>
2023-08-25 09:32:45 +0100

    Path

--- install_openjpeg.sh
+++ install_openjpeg.sh
@@ -3,8 +3,8 @@ set -x
 set -e
 
 git clone https://github.com/uclouvain/openjpeg.git --branch=v2.5.0 --depth=1
-mkdir /root/src/openjpeg/build
-cd /root/src/openjpeg/build
+mkdir /root/src/image-decoders/openjpeg/build
+cd /root/src/image-decoders/openjpeg/build
 cmake -DBUILD_JPIP=ON -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release -DBUILD_CODEC=ON -DBUILD_PKGCONFIG_FILES=ON -DBUILD_JAVA=OFF ../
 make
 make install
