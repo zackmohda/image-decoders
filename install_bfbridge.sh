cafe2f83f4601415cee523cb10aad3ba270bc4b7
CGDogan <126820728+CGDogan@users.noreply.github.com>
2023-08-25 09:28:59 +0100

    Don't save .git

--- install_bfbridge.sh
+++ install_bfbridge.sh
@@ -6,7 +6,7 @@ set -e
 git config --global advice.detachedHead false
 
 # Please remember to update version here
-git clone https://github.com/camicroscope/BFBridge.git -q --branch v0.10 --depth 1
+git clone https://github.com/camicroscope/BFBridge.git -q --branch v0.10 --depth 1 && rm -r BFBridge/.git
 
 ## BioFormats wrapper
 
