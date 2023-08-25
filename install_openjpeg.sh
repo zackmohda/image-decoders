c162083a677f7157838b2b3c5c6b130e2d7d224d
CGDogan <126820728+CGDogan@users.noreply.github.com>
2023-08-25 15:48:40 +0100

    Disable JPIP viewer

--- install_openjpeg.sh
+++ install_openjpeg.sh
@@ -5,6 +5,6 @@ set -e
 git clone https://github.com/uclouvain/openjpeg.git --branch=v2.5.0 --depth=1
 mkdir /root/src/image-decoders/openjpeg/build
 cd /root/src/image-decoders/openjpeg/build
-cmake -DBUILD_JPIP=ON -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release -DBUILD_CODEC=ON -DBUILD_PKGCONFIG_FILES=ON -DBUILD_JAVA=OFF ../
+cmake -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release -DBUILD_CODEC=ON -DBUILD_PKGCONFIG_FILES=ON -DBUILD_JAVA=OFF -DBUILD_JPIP=OFF ../
 make
 make install
