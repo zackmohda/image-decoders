28c0f874e84dfa376fbf9783cdac8d1148a7d52d
CGDogan <126820728+CGDogan@users.noreply.github.com>
2023-08-25 16:22:16 +0100

    bump bfbridge

--- install_bfbridge.sh
+++ install_bfbridge.sh
@@ -6,7 +6,7 @@ set -e
 git config --global advice.detachedHead false
 
 # Please remember to update version here
-git clone https://github.com/camicroscope/BFBridge.git -q --branch v0.10 --depth 1 && rm -r BFBridge/.git
+git clone https://github.com/camicroscope/BFBridge.git -q --branch v0.11 --depth 1 && rm -r BFBridge/.git
 
 ## BioFormats wrapper
 
