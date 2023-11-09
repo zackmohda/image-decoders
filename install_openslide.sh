fcc05a298d14101008b13f428fe9679c2b43acc8
Birm <birm@rbirm.us>
2023-11-09 15:46:50 -0500

    use openslide 4.0.0, dicom flag removed

--- install_openslide.sh
+++ install_openslide.sh
@@ -2,13 +2,12 @@
 set -x
 set -e
 
-# Use the main branch until we have a release with DICOM
-# otherwise: --branch=v3.4.1, etc.
+# v4.0.0 is the first version with dicom support. Test new versions before updating.
 # IMPORTANT NOTE: If we're building OpenSlide instead of installing from apt-get
 # whenever the version in meson.build in OpenSlide source code is updated,
 # the version in our files "libopenslide0" and "register_openslide_apt.sh"
 # needs to be updated to be a greater version number (or equal)
-git clone https://github.com/openslide/openslide.git --branch=main --depth=1
+git clone https://github.com/openslide/openslide.git --branch=v4.0.0 --depth=1
 cd openslide
 
 ## test that openslide still automatically installs libdicom as a meson submodule
@@ -20,7 +19,7 @@ test -e subprojects/libdicom.wrap
 # may need to set OPENJPEG_CFLAGS='-I/usr/local/include' and OPENJPEG_LIBS='-L/usr/local/lib -lopenjp2'
 # and the corresponding TIFF flags and libs to where bigtiff lib is installed.
 
-meson setup build_openslide -Ddicom=enabled
+meson setup build_openslide
 meson compile -C build_openslide
 meson install -C build_openslide
 rm -r build_openslide
