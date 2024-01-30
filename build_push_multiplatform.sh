c282fe362c9b3718640dda8a21d9687468e00ea3
Birm <birm@rbirm.us>
2024-01-29 19:11:57 -0500

    name image correctly on tag push

--- build_push_multiplatform.sh
+++ build_push_multiplatform.sh
@@ -1,3 +1,3 @@
 docker buildx create --use
-docker buildx build --platform linux/amd64,linux/arm64 -t camicroscope/image_decoders -f Dockerfile --push .
+docker buildx build --platform linux/amd64,linux/arm64 -t camicroscope/image-decoders -f Dockerfile --push .
 docker push camicroscope/image_decoders
