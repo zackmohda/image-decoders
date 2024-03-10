f43c8ac12dc26f7e70a75ded31efb329e0757119
Birm <birm@rbirm.us>
2024-03-10 03:25:13 -0400

    Update openslide_symlink.sh

--- openslide_symlink.sh
+++ openslide_symlink.sh
@@ -5,8 +5,9 @@ if [ -n "$lib_path" ]; then
     # Navigate to the directory containing libopenslide.so.1
     cd "$(dirname "$lib_path")"
 
-    # Create a symbolic link from libopenslide.so.1 to libopenslide.so.0
+    # Create a symbolic link from libopenslide.so.1 to libopenslide.so.0, and another in /usr/local/lib directly
     ln -s libopenslide.so.1 libopenslide.so.0
+    ln -s libopenslide.so.1 /usr/local/lib/libopenslide.so.0
 else
     echo "libopenslide.so.1 not found in /usr/local/lib/"
 fi
